f0a5d5caf24bc8e5fe228e4b4c7fae91436a9231
juan sebastian alvarez <juan.s.alvarez563@gmail.com>
2023-12-01 14:20:30 -0500

    Realizacion de modelo de notas

--- appnotas/Notas/models.py
+++ appnotas/Notas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Nota (models.Model):
+    
+
+    """
+    Modelo que representa de los elemnetos de las notas las cuales se veran en app notas
+    """
+    Titulo = models.CharField(max_length=30)
+    Nota =  models.PositiveSmallIntegerField()
+    Fecha = models.DateField()
+    
+    def __str__(self) :
+        return self.Titulo
+   
+    
+
